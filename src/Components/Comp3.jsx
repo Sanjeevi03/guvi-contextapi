1be3b4ff8b63f34163faf5d9e91eacb2caea961a
sanjeevi03 <sanjeevisan65@gmail.com>
2022-02-10 14:21:24 +0530

    commit on feb 10 2pm

--- src/Components/Comp3.jsx
+++ src/Components/Comp3.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
-
-function Comp3() {
-  return <div></div>;
+import React from "react";
+import { Context } from "./Context";
+class Comp3 extends React.Component {
+  static contextType = Context;
+  render() {
+    return (
+      <>
+        <h3>Component 3</h3>
+        <h4>Count: {this.context.count}</h4>
+        <button onClick={this.context.handleIncrement}> Increment </button>{" "}
+        &nbsp;
+        <button onClick={this.context.handleDecrement}> Decrement </button>{" "}
+        <button onClick={this.context.handleReset}>Reset</button>
+        &nbsp;
+      </>
+    );
+  }
 }
 
 export default Comp3;
